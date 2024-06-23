1e76c36dddeb980b8c6f15aee0fc48742c20dbbb
Ilham Zukhri <112614524+ilhamZ354@users.noreply.github.com>
2024-06-23 09:16:53 +0700

    update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,47 +14,48 @@
                         </div>
                     </div>
                     <div class="card-body px-lg-5 py-lg-5">
-                        <form role="form" method="POST" action="{{ route('login') }}">
-                            @csrf
+                    <form role="form" method="POST" action="{{ route('login') }}">
+                                    @csrf
 
-                            <div class="form-group{{ $errors->has('username') ? ' has-danger' : '' }} mb-3">
-                            <label for="username"><small>Username</small></label>
-                                <div class="input-group input-group-alternative">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="ni ni-circle-08"></i></span>
+                                    <div class="form-group{{ $errors->has('username') ? ' has-danger' : '' }} mb-3">
+                                        <div class="input-group input-group-alternative">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="ni ni-circle-08"></i></span>
+                                            </div>
+                                            <input class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" placeholder="NIP/NISN" type="text" name="username" value="{{ old('username') }}" required autofocus>
+                                        </div>
+                                        @if ($errors->has('username'))
+                                            <span class="invalid-feedback" style="display: block;" role="alert">
+                                                <strong>{{ $errors->first('username') }}</strong>
+                                            </span>
+                                        @endif
                                     </div>
-                                    <input class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" placeholder="{{ __('NIP/NISN') }}" type="text" name="username" value="{{ old('username') }}" value="1234" required autofocus>
-                                </div>
-                                @if ($errors->has('username'))
-                                    <span class="invalid-feedback" style="display: block;" role="alert">
-                                        <strong>{{ $errors->first('username') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                            <label for="password"><small>Password</small></label>
-                            <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
-                                <div class="input-group input-group-alternative">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+
+                                    <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
+                                        <div class="input-group input-group-alternative">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                                            </div>
+                                            <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="{{ __('Password') }}" type="password" value="secret" required>
+                                        </div>
+                                        @if ($errors->has('password'))
+                                            <span class="invalid-feedback" style="display: block;" role="alert">
+                                                <strong>{{ $errors->first('password') }}</strong>
+                                            </span>
+                                        @endif
                                     </div>
-                                    <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="{{ __('Password') }}" type="password" value="secret" required>
-                                </div>
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" style="display: block;" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                            <div class="custom-control custom-control-alternative custom-checkbox">
-                                <input class="custom-control-input" name="remember" id="customCheckLogin" type="checkbox" {{ old('remember') ? 'checked' : '' }}>
-                                <label class="custom-control-label" for="customCheckLogin">
-                                    <span class="text-muted">{{ __('Remember me') }}</span>
-                                </label>
-                            </div>
-                            <div class="text-center">
-                                <button type="submit" class="btn btn-primary my-4">{{ __('Sign in') }}</button>
-                            </div>
-                        </form>
+
+                                    <div class="custom-control custom-control-alternative custom-checkbox">
+                                        <input class="custom-control-input" name="remember" id="customCheckLogin" type="checkbox" {{ old('remember') ? 'checked' : '' }}>
+                                        <label class="custom-control-label" for="customCheckLogin">
+                                            <span class="text-muted">{{ __('Remember me') }}</span>
+                                        </label>
+                                    </div>
+                                    <div class="text-center">
+                                        <button type="submit" class="btn btn-primary my-4">{{ __('Sign in') }}</button>
+                                    </div>
+                                </form>
+
                             <div class="text-center">
                                 <a href="#"><small>Login sebagai orangtua</small></a>
                             </div>
